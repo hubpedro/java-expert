456c706ef68eb4183214dae3f99f293c37af5d39
Pedro Barbosa <pedro.barbosa.dev@gmail.com>
2025-11-09 10:25:34 -0300

    Exercise 02 - ref #9

---
+++ src/Fundamentals/ExerciseTwo.java
@@ -0,0 +1,26 @@
+package Fundamentals;
+
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ExerciseTwo{
+    /*
+     * Exercício 2: Adicionar Dinamicamente
+        Crie um ArrayList vazia.
+        * Peça ao usuário para digitar 5 nomes de cidades.
+        * Adicione cada uma à lista.
+        * Por fim, imprima todas as cidades adicionadas.
+        * O que você está praticando: Entrada do usuário, add()
+        * Desafio: Use um loop para ler 5 vezes em vez de escrever 5 vezes
+     */
+    void main() {
+        ArrayList<String> cities = new ArrayList<String>(5);
+        Scanner scanner = new Scanner(System.in);
+        for(int i = 0; i < 5; i++){
+            System.out.print("Enter City Name: ");
+            cities.add(scanner.nextLine());
+        }
+        cities.forEach(System.out::println);
+    }
+}
