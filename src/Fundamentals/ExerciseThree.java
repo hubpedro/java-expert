28b50dd7cbc2ca2e603a0db148a0e9ad85fbfa09
Pedro Barbosa <pedro.barbosa.dev@gmail.com>
2025-11-09 11:06:28 -0300

    exercise 03 ref #

---
+++ src/Fundamentals/ExerciseThree.java
@@ -0,0 +1,33 @@
+package Fundamentals;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.function.BiPredicate;
+import java.util.function.IntSupplier;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class ExerciseThree {
+    /* Crie uma ArrayList com 10 números inteiros (1-10).
+     *  Peça ao usuário para digitar um número.
+     *  Verifique se esse número existe na lista usando contains().
+     *
+     */
+    void main() {
+        ArrayList<Integer> data = IntStream.generate(() -> new Random().nextInt(0, 99)).limit(100).boxed().collect(Collectors.toCollection(ArrayList::new));
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter an integer between 1 and 100: ");
+        int userNumber = Integer.parseInt(sc.nextLine());
+
+        BiPredicate<ArrayList<Integer>, Integer> checkIfNumberExists = ArrayList::contains;
+
+        boolean result = checkIfNumberExists.test(data, userNumber);
+
+        System.out.printf("Does %d inside the ArrayList: %s%n", userNumber, result);
+
+        System.out.print(data);
+    }
+
+}
