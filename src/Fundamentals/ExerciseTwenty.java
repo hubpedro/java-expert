a5a460d4d384a72c7aa238a6322e5de88c92939a
Pedro Barbosa <pedro.barbosa.dev@gmail.com>
2025-11-10 13:10:11 -0300

    Exercise 20

---
+++ src/Fundamentals/ExerciseTwenty.java
@@ -0,0 +1,42 @@
+package Fundamentals;
+
+import java.util.ArrayList;
+
+public class ExerciseTwenty {
+    void main() {
+        int size = 5;
+        ArrayList<Product> products = new ArrayList<Product>(size);
+        for (int i = 0; i < size; i++) {
+            products.add(new Product("Random product", 50.00));
+        }
+
+        System.out.println(calculatePrice(products));
+
+    }
+
+    double calculatePrice(ArrayList<Product> products) {
+        return products.stream()
+                .reduce(0.0,
+                        (acc, p) -> acc + p.getPrice(),
+                        Double::sum);
+    }
+
+    class Product {
+        String name;
+        double price;
+
+        public Product(String name, double price) {
+            this.name = name;
+            this.price = price;
+        }
+
+        public double getPrice() {
+            return this.price;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("Person[Name: %s, Price: %s]", name, price);
+        }
+    }
+}
