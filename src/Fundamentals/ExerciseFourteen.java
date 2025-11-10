064ea5ea899031f21dfcf07b90db4a50a12d4c29
Pedro Barbosa <pedro.barbosa.dev@gmail.com>
2025-11-10 09:39:43 -0300

    Exercise Fourteen ref #21

--- src/Fundamentals/ExerciseFourteen.java
+++ src/Fundamentals/ExerciseFourteen.java
@@ -1,4 +1,15 @@
 package Fundamentals;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class ExerciseFourteen {
+    void main() {
+        ArrayList<Integer> numbers = new ArrayList<>(List.of(15, 3, 42, 7, 1));
+        System.out.println("Before:" + numbers);
+        Collections.sort(numbers);
+        System.out.println("Calling Collections.sort");
+        System.out.println("After:" + numbers);
+    }
 }
