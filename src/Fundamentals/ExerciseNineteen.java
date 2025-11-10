2adc613cfb052b0bcdc149d0cf12b0d8f3db3126
Pedro Barbosa <pedro.barbosa.dev@gmail.com>
2025-11-10 11:22:23 -0300

    Exercise 19 ref #26

---
+++ src/Fundamentals/ExerciseNineteen.java
@@ -0,0 +1,28 @@
+package Fundamentals;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ExerciseNineteen {
+    void main() {
+        ArrayList<Integer> numbers = new ArrayList<>(List.of(45, 12, 78, 3, 99, 22));
+
+        System.out.println(numbers);
+
+        int maxNumber = Collections.max(numbers);
+        int minNumber = Collections.min(numbers);
+
+        System.out.println("maxNumber = " + maxNumber);
+        System.out.println("minNumber = " + minNumber);
+
+        numbers.clear();
+        System.out.println("ArrayList<Integer> numbers after clear() " + numbers);
+        System.out.println("NoSuchElementException");
+//        int max = Collections.max(numbers);
+//        int min = Collections.min(numbers);
+
+//        System.out.println("max = " + max);
+//        System.out.println("min = " + min);
+    }
+}
