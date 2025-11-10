c651b9a59a348c418f5f55d3e1880c410b6af2f9
Pedro Barbosa <pedro.barbosa.dev@gmail.com>
2025-11-09 21:17:39 -0300

    Exercise Twelve ref #19

--- src/Fundamentals/ExerciseTwelve.java
+++ src/Fundamentals/ExerciseTwelve.java
@@ -1,4 +1,17 @@
 package Fundamentals;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ExerciseTwelve {
+    void main() {
+        ArrayList<String> fruits = new ArrayList<>(List.of("Apple", "Banana", "Orange", "Watermelon"));
+        System.out.println("Before " + fruits);
+
+        boolean someFruitHasBeenRemoved = fruits.removeIf(fruit -> fruit.toLowerCase().startsWith("b"));
+
+        System.out.println("One fruit was removed: " + someFruitHasBeenRemoved);
+
+        System.out.println("After " + fruits);
+    }
 }
