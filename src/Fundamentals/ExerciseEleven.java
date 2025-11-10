3db9b14542bdab6267b9d71ee67d705bbeb2bccc
Pedro Barbosa <pedro.barbosa.dev@gmail.com>
2025-11-09 21:15:50 -0300

    Exercise Eleven ref #18

---
+++ src/Fundamentals/ExerciseEleven.java
@@ -0,0 +1,14 @@
+package Fundamentals;
+
+import java.util.ArrayList;
+
+public class ExerciseEleven {
+    void main() {
+        ArrayList<Integer> numbers = new ArrayList<>(0);
+        System.out.println("Before " + numbers);
+        IO.println("The ArrayList is empty: " + numbers.isEmpty());
+        numbers.add(1);
+        IO.println("The ArrayList is empty: " + numbers.isEmpty());
+        System.out.println("After " + numbers);
+    }
+}
